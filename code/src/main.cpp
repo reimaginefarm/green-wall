ef0b7e36c2496b380aa20a349f98bb5e9e32e3e9
niniack <nsa325@nyu.edu>
2019-09-02 21:23:06 +0400

    failed attempt ota code

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,10 +1,66 @@
-#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <ArduinoOTA.h>
+
+ESP8266WiFiMulti wifiMulti;     // Create an instance of the ESP8266WiFiMulti class, called 'wifiMulti'
+
+const byte led = 2;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);         // Start the Serial communication to send messages to the computer
+  delay(10);
+  Serial.println('\n');
+
+  wifiMulti.addAP("2.4g", "dauyndauyn");   // add Wi-Fi networks you want to connect to
+
+  Serial.println("Connecting ...");
+  int i = 0;
+  while (wifiMulti.run() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
+    delay(250);
+    Serial.print('.');
+  }
+  Serial.println('\n');
+  Serial.print("Connected to ");
+  Serial.println(WiFi.SSID());              // Tell us what network we're connected to
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());           // Send the IP address of the ESP8266 to the computer
+
+  ArduinoOTA.setHostname("slaveUnit");
+  ArduinoOTA.setPassword("dauyndauyn");
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("OTA ready");
+
+  pinMode(led, OUTPUT);
+  digitalWrite(led, 1);
 }
 
+unsigned long previousTime = millis();
+
+const unsigned long interval = 1000;
+
 void loop() {
-  delay(1000);
-  Serial.println("Hello World!");
+  ArduinoOTA.handle();
+  unsigned long diff = millis() - previousTime;
+  if(diff > interval) {
+    digitalWrite(led, !digitalRead(led));  // Change the state of the LED
+    previousTime += diff;
+  }
 }
