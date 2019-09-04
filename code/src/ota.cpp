159323d9c7173178f162951438848b10e97a9f8b
niniack <nsa325@nyu.edu>
2019-09-04 22:28:21 +0400

    separated ota module from main

---
+++ code/src/ota.cpp
@@ -0,0 +1,58 @@
+// reimagine.farm
+// OTA MODULE
+// Copyright (C) 2019 by reimagine.farm <contact at reimagine dot farm>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <ArduinoOTA.h>
+#include <iostream>
+
+void otaSetup() {
+
+  ESP8266WiFiMulti wifiMulti;     // Create an instance of the ESP8266WiFiMulti class, called 'wifiMulti'
+
+  Serial.begin(115200);         // Start the Serial communication to send messages to the computer
+  delay(10);
+  Serial.println('\n');
+
+  wifiMulti.addAP("2.4g", "dauyndauyn");   // add Wi-Fi networks you want to connect to
+
+  Serial.println("Connecting ...");
+  int i = 0;
+  while (wifiMulti.run() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
+    delay(250);
+    Serial.print('.');
+  }
+  Serial.println('\n');
+  Serial.print("Connected to ");
+  Serial.println(WiFi.SSID());              // Tell us what network we're connected to
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());           // Send the IP address of the ESP8266 to the computer
+
+  ArduinoOTA.setHostname("slaveUnit");
+  //ArduinoOTA.setPassword("dauyndauyn");
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("OTA ready");
+}
+
+void otaLoop(){
+  ArduinoOTA.handle();
+}
---
+++ code/src/ota.hpp
@@ -0,0 +1,11 @@
+// reimagine.farm
+// OTA MODULE
+// Copyright (C) 2019 by reimagine.farm <contact at reimagine dot farm>
+
+#ifndef OTA_HPP
+#define OTA_HPP
+
+void otaSetup();
+void otaLoop();
+
+#endif
