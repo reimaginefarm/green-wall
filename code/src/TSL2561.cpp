d286eb2bc25bbc8d28bc0e09692ae6574db95b6d
niniack <nsa325@nyu.edu>
2019-09-05 18:40:32 +0400

    code correction for light sensor

--- code/src/TSL2561.cpp
+++ code/src/TSL2561.cpp
@@ -2,13 +2,15 @@
 // Lights (TSL2561) Class Implementation
 // Copyright (C) 2019 by reimagine.farm <contact at reimagine dot farm>
 
+#include "TSL2561.hpp"
+
 
 TSL2561 ::  TSL2561() : name("light_sensor"), version(sensor.version),
 id(sensor.sensor_id), maximumValue(sensor.max_value),
 minimumValue(sensor.min_value), resolution(sensor.resolution),
 delay(sensor.min_delay), unit("lux"){
 
-};
+}
 
 void TSL2561 :: configureSensor() {
   /* You can also manually set the gain or enable auto-gain support */
@@ -20,11 +22,11 @@ void TSL2561 :: configureSensor() {
   tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_13MS);      /* fast but low resolution */
   // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_101MS);  /* medium resolution and speed   */
   // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_402MS);  /* 16-bit data but slowest conversions */
-};
+}
 
 void TSL2561 :: sensorSetup() {
   tsl.begin();
-};
+}
 
 void TSL2561 :: getMeasurement() {
   sensors_event_t event;
